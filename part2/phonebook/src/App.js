c18a9732b71e47b27957e8687efd7c6649305546
joerhew <joerhew@gmail.com>
2023-06-21 19:43:48 -0400

    finished ex2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({name: newName}));
+    setNewName('');
+  }
+
+  const typeNewName = (event) => {
+    console.log('typing: ', event.target.value);
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={typeNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons}/>
+    </div>
+  )
+}
+
+const Persons = ({ persons }) => {
+  return persons.map(person => 
+    <Person key={person.name} person={person} />
+  )
+}
+
+const Person = ({ person }) => (
+  <>
+    <p>{person.name}</p>
+  </>
+)
+
+export default App
