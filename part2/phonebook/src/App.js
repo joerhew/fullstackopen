6f12b600d7eaf020c9467a2d73089bb524f7d61d
joerhew <joerhew@gmail.com>
2023-06-22 09:53:16 -0400

    Finished ex2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: 'Arto Hellas', phone: '416 123 4567' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    if (persons.some(person => person.name === newName)) { //If the person's name is already in the phonebook
+      alert(`${newName} is already added to phonebook`); //Alert
     } else {
-      setPersons(persons.concat({name: newName}));
-      setNewName('');
+      setPersons(persons.concat({name: newName, phone: newPhone})); //Add new person
+      setNewName(''); //Set name input to blank
+      setNewPhone(''); //Set phone input to blank
     };
   }
 
@@ -21,12 +23,18 @@ const App = () => {
     setNewName(event.target.value);
   }
 
+  const typeNewPhone = (event) => {
+    console.log('typing: ', event.target.value);
+    setNewPhone(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={typeNewName}/>
+          name: <input value={newPhone} onChange={typeNewPhone}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -46,7 +54,7 @@ const Persons = ({ persons }) => {
 
 const Person = ({ person }) => (
   <>
-    <p>{person.name}</p>
+    <p>{person.name} {person.phone}</p>
   </>
 )
 
