cf464e299b2c356e90339ef03b2d7b7f8552e370
joerhew <joerhew@gmail.com>
2023-10-12 11:03:51 -0400

    Finished ex6.24

--- part6/hook-counter/src/App.jsx
+++ part6/hook-counter/src/App.jsx
@@ -1,31 +1,15 @@
-import { useReducer } from 'react'
+import Display from './components/Display'
+import Button from './components/Button'
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-        return state + 1
-    case "DEC":
-        return state - 1
-    case "ZERO":
-        return 0
-    default:
-        return state
-  }
-}
-
-const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
-  return (
+const App = () => (
+  <div>
+    <Display />
     <div>
-      <div>{counter}</div>
-      <div>
-        <button onClick={() => counterDispatch({ type: "INC"})}>+</button>
-        <button onClick={() => counterDispatch({ type: "DEC"})}>-</button>
-        <button onClick={() => counterDispatch({ type: "ZERO"})}>0</button>
-      </div>
+      <Button type="INC" label="+" />
+      <Button type="DEC" label="-" />
+      <Button type="ZERO" label="0" />
     </div>
-  )
-}
+  </div>  
+)
 
 export default App
